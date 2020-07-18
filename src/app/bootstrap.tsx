e4ea1928c62d827acde51934729683aba297d757
zx <zxpstudy@163.com>
2020-07-19 00:00:46 +0800

    feat: add script input

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -1,13 +1,19 @@
 import { createBootstrap } from "../core/bootstrap";
-import { conf } from "./config";
+import { APP_CONFIG } from "./config";
 import App from "./App";
 import React from "react";
 import { CSSPreset } from "./CSSPreset";
+import { ThemeProvider } from "../core/style/Theme";
+import { confLoader } from "../core/config";
+
+const conf = confLoader<keyof typeof APP_CONFIG>();
 
 const SetUp = () => (
-  <CSSPreset>
-    <App />
-  </CSSPreset>
+  <ThemeProvider>
+    <CSSPreset>
+      <App />
+    </CSSPreset>
+  </ThemeProvider>
 );
 
 createBootstrap(conf())(() => <SetUp />, document.getElementById("root") as Element);
